96bfaa009797a3cbc09a2c73cdf81048eca5c60c
naveen kumar n <naveennaviaroo841@gmail.com>
2023-06-11 21:53:31 +0530

    my first commit

---
+++ src/Components/Categories.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+import {categories} from "../data"
+import CategoriesItem from './CategoriesItem'
+import {mobile} from "../Responsive"
+const Container=styled.div`
+display:flex;
+padding:5px;
+justify-content: space-between;
+${mobile({padding:"0px",flexDirection:"column"})}
+`;
+
+function Categories() {
+  return (
+    <Container>
+      {categories.map(item=>(
+        <CategoriesItem item={item} key={item.id}/>
+      ))}
+    </Container>
+  )
+}
+
+export default Categories
